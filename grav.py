35de9cfc224f58a0adbe49a709b73665085292c9
Andy McAliley <andy.mcaliley@gmail.com>
2016-01-02 01:05:27 -0700

    debugging

--- grav.py
+++ grav.py
@@ -10,7 +10,7 @@ import scipy as sc
 import scipy.sparse as sparse
 import numpy as np
 
-def gc3D( x,y,z ):
+def gc3d( x,y,z ):
    "function_docstring"
    #geometric factor for obs point at origin
    #multiply by gamma*rho for g
@@ -18,12 +18,12 @@ def gc3D( x,y,z ):
    
    r=math.sqrt(x**2 + y**2 + z**2)
    #Blakely
-   #result=z*math.atan2(x*y,z*r) - x*math.log(r+y) - y*math.log(r+x)
+   result=z*math.atan2(x*y,z*r) - x*math.log(r+y) - y*math.log(r+x)
    #Okabe
-   result=2*z*math.atan2(x+y+r,z) + x*math.log(r+y) + y*math.log(r+x)
+   #result=2*z*math.atan2(x+y+r,z) + x*math.log(r+y) + y*math.log(r+x)
    return result
 
-def g_3DRect( xlims,ylims,zlims ):
+def g_3drect( xlims,ylims,zlims ):
    "function_docstring"
    #geometric factor for obs point at origin
    #multiply by gamma*rho for gz
@@ -34,16 +34,16 @@ def g_3DRect( xlims,ylims,zlims ):
        for jj in range(1,2):
            for kk in range(1,2):
                mu = (-1)**ii*(-1)**jj*(-1)**kk
-               sum = sum + mu*gc3D(xlims[ii-1],ylims[jj-1],zlims[kk-1])
+               sum = sum + mu*gc3d(xlims[ii-1],ylims[jj-1],zlims[kk-1])
    return sum
 
-def gc2D( x,z ):
+def gc2d( x,z ):
     #A corner of a 2D rectangle (infinite extent in y)
     
     result=x*math.log(x**2+z**2)+2*z*math.atan2(x,z)
     return result
 
-def g_2DRect( xlims,zlims ):
+def g_2drect( xlims,zlims ):
     #geometric factor for a rectangle (infinite extent in y)
     #obs point at origin
     #multiply by gamma*rho for gz
@@ -58,25 +58,25 @@ def g_2DRect( xlims,zlims ):
     for ii in range(1,2):
         for jj in range(1,2):
             mu=(-1)**ii*(-1)**jj
-            sum=sum+mu*gc2D(xlims[ii-1],zlims[jj-1])
+            sum=sum+mu*gc2d(xlims[ii-1],zlims[jj-1])
     return sum
 
-def g_2DMesh( xlims,zlims ):
+def g_2dmesh( xnodes,znodes ):
     #geometric factors for a 2D mesh
     #obs point at origin
     #multiply by gamma*rho for gz
     
     #get dimensions
-    nx=len(xlims)
-    nz=len(zlims)
+    nx=len(xnodes)
+    nz=len(znodes)
     #construct matrix to calculate factors from corners
     corners2factors=sparse.kron(spFD1D(nx),spFD1D(nz))
     #initialize corners vector
     corners=np.zeros((1,nx*nz))
     i=0
-    for x in xlims:
-        for z in zlims:
-            corners[i]=gc2D( x,z )
+    for x in xnodes:
+        for z in znodes:
+            corners[i]=gc2d( x,z )
             i+=1
     factors=corners2factors.dot(corners)
     return factors
@@ -89,8 +89,14 @@ def spFD1D(n):
     e[0]=-e[0]
     mat=sparse.diags(e,[0,1],shape=[n-1,n])
     return mat
-#test
-xx=(1.,3.)
-yy=(1.,2.)
-zz=(0.5,2.)
-answer=gz_rectPrism(xx,yy,zz)
+
+def main():
+    #test
+    xx=(1,3)
+    yy=(1,2)
+    zz=(105,120)
+    answer=g_3drect(xx,yy,zz)
+    print(answer)
+
+if __name__=='__main__':
+    main()
